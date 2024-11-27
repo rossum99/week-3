205f62d88044c2cc90ee716567bb9991719fab77
rossum99 <108015375+rossum99@users.noreply.github.com>
2024-11-27 13:11:22 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Odd numbers from 1 to %d are:\n", n);
+    for (int i = 1; i <= n; i += 2) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
